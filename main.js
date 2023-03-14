987312658cb65968f0e7488ec84dec40b473ba4e
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-03-14 10:41:18 -0300

    segunda pree entrega

--- main.js
+++ main.js
@@ -112,6 +112,7 @@ while (continuar) {
 			let listaProductos = "";
 			// Variable i para que ponga a modo de lista los productos
 			let i = 1;
+			// Función de orden superior para recorrer el array de los productos y ponerlos en forma de lista
 			prod.forEach((producto) => {
 				listaProductos += `${i} - ${producto.nombre} $${producto.precio}\n`;
 				i++;
@@ -168,7 +169,7 @@ while (continuar) {
 				// Función de orden superior sort() para ordenar los productos del carrito de menor a mayor
 				carrito.sort((a, b) => b.precio - a.precio);
 				let mensaje = "Carrito:\n";
-				// Función de orden superior para
+				// Función de orden superior para recorrer los productos agregados al carrito y mostrarlos en forma de lista, ordenados de mayor a menor
 				carrito.forEach((producto) => {
 					mensaje += `${producto.nombre} - $${producto.precio}\n`;
 					// Sumar el precio del producto al total
